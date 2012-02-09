614986638c47c79c0a086eadd51afa5bf4013c2d
Ravi Nanavati <ravi_n@alum.mit.edu>
2012-02-09 12:24:05 +0800

    Remove more functions that aren't part of LLVM 3.0

--- base/LLVM/Target/PIC16.hs
+++ base/LLVM/Target/PIC16.hs
@@ -1,10 +1,14 @@
-{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE ForeignFunctionInterface, CPP #-}
 module LLVM.Target.PIC16(initializeTarget) where
 
 initializeTarget :: IO ()
+#if HS_LLVM_VERSION < 300
 initializeTarget = do
     initializePIC16TargetInfo
     initializePIC16Target
 
 foreign import ccall unsafe "LLVMInitializePIC16TargetInfo" initializePIC16TargetInfo :: IO ()
 foreign import ccall unsafe "LLVMInitializePIC16Target" initializePIC16Target :: IO ()
+#else
+initializeTarget = return ()
+#endif
